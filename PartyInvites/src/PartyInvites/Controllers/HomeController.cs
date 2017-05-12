0977a963aefe55fbdee8a9cc4f725d3360f56c3a
liefu.zhang <liefu.zhang@WSAKL001173>
2017-05-12 16:42:26 +1200

    listing 2-17

--- PartyInvites/src/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -1,35 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
 
-namespace PartyInvites.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
