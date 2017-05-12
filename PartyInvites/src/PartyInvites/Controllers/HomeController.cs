b2ce0b0fde248918a8febfe15e0846da2639891f
liefu zhang <liefuzhangnz@gmail.com>
2017-05-12 22:29:02 +1200

    final commit

--- PartyInvites/src/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -18,8 +18,14 @@ namespace PartyInvites.Controllers {
 
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse) {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else {
+                // there is a validation error
+                return View();
+            }
         }
 
         public ViewResult ListResponses() {
--- PartyInvites/src/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/src/PartyInvites/Models/GuestResponse.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models {
-    public class GuestResponse {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public bool? WillAttend { get; set; }
-    }
+  public class GuestResponse {
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter your email address")]
+    [RegularExpression(".+\\@.+\\..+",
+    ErrorMessage = "Please enter a valid email address")]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Please enter your phone number")]
+    public string Phone { get; set; }
+    [Required(ErrorMessage = "Please specify whether you'll attend")]
+    public bool? WillAttend { get; set; }
+  }
 }
