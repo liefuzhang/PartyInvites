b7844185d57c388a55704db3b60fc93dcdb0b09e
liefu zhang <liefuzhangnz@gmail.com>
2017-05-14 20:33:53 +1200

    5-14

---
+++ PartyInvites/src/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+        }
+    }
+}
